f9a0ae70458678434d4603e9c4495dff1757b9b4
Mark Steve Samson <hello@marksteve.com>
2022-01-08 13:56:59 +0800

    Setup library config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.tsx'),
+      name: 'usapan',
+      fileName: (format) => `usapan.${format}.js`,
+    },
+  },
 })
